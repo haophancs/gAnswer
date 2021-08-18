3c314fd92f16b23304e52c1a4500c6747e8d3d7c
haophancs <haophan.cs.uit@gmail.com>
2021-08-19 00:48:59 +0300

    update

--- init.sh
+++ init.sh
@@ -1,32 +1,22 @@
-FROM ubuntu:20.04
-RUN mkdir -p /gAnswer
-WORKDIR /gAnswer
-VOLUME . /gAnswer
-
-RUN apt update \
+apt update \
     && apt autoclean \
     && apt install -y tmux vim wget git \
     && apt install -y python3 python3-pip \
     && apt install -y openjdk-8-jdk openjdk-8-jre \
-    && apt install -y upgrade
+    && apt upgrade -y
 
-RUN pip install -y gdown
+pip install gdown
 
-RUN mkdir -p /gAnswer/data \
-    && cd /gAnswer/data \
+mkdir -p ./data \
+    && cd ./data \
     && gdown https://drive.google.com/u/0/uc?id=1hmqaftrTo0_qQNRApCuxFXaBx7SosNVy \
     && unrar x ./DBpedia2016.rar ./ \
     && rm ./DBpedia2016.rar \
-    && cd /gAnswer
+    && cd ..
 
-RUN gdown https://drive.google.com/u/0/uc?id=1tEsi4pBOBHd2gmwVgIOgt-ypJZQH9G3S \
+gdown https://drive.google.com/u/0/uc?id=1tEsi4pBOBHd2gmwVgIOgt-ypJZQH9G3S \
     && unzip ganswer_lib.zip \
     && mv ganswer_lib lib \
     && rm ganswer_lib.zip
 
-RUN chmod +rx ./run_http.sh
-
-EXPOSE 9999
-EXPOSE 9000
-
-CMD ["./run_http.sh"]
+chmod +rx ./run_http.sh
