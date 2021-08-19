17048ae2f31ae89300a2ca4e7b15621ff7caec8d
haophancs <haophan.cs.uit@gmail.com>
2021-08-19 14:16:56 +0300

    update

--- src/jgsc/GstoreConnector.java
+++ src/jgsc/GstoreConnector.java
@@ -52,7 +52,6 @@ public class GstoreConnector {
             System.out.println("request: "+urlNameString);
             URL realUrl = new URL(urlNameString);
             HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
-            HttpURLConnection.setFollowRedirects(false);
             connection.setConnectTimeout(10000);
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
@@ -117,7 +116,8 @@ public class GstoreConnector {
             String urlNameString = url + "/" + param;
             System.out.println("request: "+urlNameString);
             URL realUrl = new URL(urlNameString);
-            URLConnection connection = realUrl.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
+            connection.setConnectTimeout(10000);
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
             //set agent to avoid: speed limited by server if server think the client not a browser
